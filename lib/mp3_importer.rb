dc5c77b6a193f51d06813c8a5e3158e5e1ce9812
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-23 01:55:11 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer
+  
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    new_list = []
+    list = Dir.entries('./spec/fixtures/mp3s')
+    list.each do |a_list| 
+      if a_list.end_with? ".mp3"
+        new_list << a_list
+      end
+    end
+    new_list
+  end
+  
+end
