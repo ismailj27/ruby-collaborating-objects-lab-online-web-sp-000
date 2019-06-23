5d3a36aa6698e21cc89ed52385f268ba0fe07bf4
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-23 02:00:14 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,15 +7,15 @@ class MP3Importer
   end
   
   def files
-    @new_list = []
+    @ew_list = []
     list = Dir.entries('./spec/fixtures/mp3s')
     list.each do |a_list| 
       if a_list.end_with? ".mp3"
-        @new_list << a_list
+        new_list << a_list
       end
     end
     binding.pry
-    @new_list
+    new_list
   end
   
   def import
