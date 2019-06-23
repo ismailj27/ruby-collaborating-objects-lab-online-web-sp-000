f5f1597f30d6b4d90146b97c70d5bf16e4629ece
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-23 02:41:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,6 @@ class Song
   def self.new_by_filename(file_name)
     song = self.new(file_name.split(" - ")[1])
     song
-    Song.artist.name = file_name.split(" - ")[0]
-    song
   end
   
 end
