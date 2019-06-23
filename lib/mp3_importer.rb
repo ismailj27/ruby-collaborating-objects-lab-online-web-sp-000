ceba7af7316cbeff817ddfb1f64d097361e439fb
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-23 01:52:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,8 +8,8 @@ class MP3Importer
   
   def files
     list = Dir.entries('./spec/fixtures/mp3s')
-    list.each do |person| 
-      puts "#{person.name}"
+    list.each do |a_list| 
+      if a_list
     end
   end
   
