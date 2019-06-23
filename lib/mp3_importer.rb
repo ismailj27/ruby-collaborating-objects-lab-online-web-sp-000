0d9be9e152a8253c8a4c22006fa1118c9af6903c
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-23 02:37:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,18 +7,18 @@ class MP3Importer
   end
   
   def files
-    @new_list = []
+    @@new_list = []
     list = Dir.entries('./spec/fixtures/mp3s')
     list.each do |a_list| 
       if a_list.end_with? ".mp3"
-        @new_list << a_list
+        @@new_list << a_list
       end
     end
-    @new_list
+    @@new_list
   end
   
   def import
-    @new_list.each do |d_filename|
+    @@new_list.each do |d_filename|
       song.new_by_filename(d_filename)
     end
   end
